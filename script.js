7c567b5150b5b35b7fcab1818a21321d5c416978
Diego Mendes da Silva <diegomendes90@gmail.com>
2019-07-16 12:51:01 -0300

    commit

--- script.js
+++ script.js
@@ -14,11 +14,41 @@ function verificar(){
         var valorsexo = document.getElementsByName('selsexo')
         var idade = ano - Number(valordoano.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if(valorsexo[0].checked) {
             genero = 'Homem'
+            if (idade >= 0 && idade <= 10){
+                //CRIANÇA
+                img.setAttribute('src', '_imagem/crianca_h.png')
+            } else if (idade < 21) {
+                //JOVEM
+                img.setAttribute('src', '_imagem/jovem_h.png')
+            } else if (idade < 50) {
+                //ADULTO
+                img.setAttribute('src', '_imagem/adulto_h.png')
+            } else {
+                //IDOSO
+                img.setAttribute('src', '_imagem/velho_h.png')
+            }
         } else if (valorsexo[1].checked) {
             genero = 'Mulher'
+            if (idade >= 0 && idade <= 10){
+                //CRIANÇA
+                img.setAttribute('src', '_imagem/crianca_f.png')
+            } else if (idade < 21) {
+                //JOVEM
+                img.setAttribute('src', '_imagem/jovem_f.png')
+            } else if (idade < 50) {
+                //ADULTO
+                img.setAttribute('src', '_imagem/adulto_f.png')
+            } else {
+                //IDOSO
+                img.setAttribute('src', '_imagem/velho_f.png')
+            }
         }
         resultadofinal.style.textAlign = 'center'
-        resultadofinal.innerHTML = `<h3>Detectamos ${genero} com idade de ${idade}</h3>`   }
+        resultadofinal.innerHTML = `<h1>Detectamos ${genero} com idade de ${idade}</h1>`
+        resultadofinal.appendChild(img)
+    }
 }
